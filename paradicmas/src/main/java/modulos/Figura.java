c4cf255cfe9945510a832881b7173ec37ef1fc5c
julianmurillo601-cmd <julianmurillo601@gmail.com>
2026-03-23 18:23:50 -0500

    first commit

---
+++ paradicmas/src/main/java/modulos/Figura.java
@@ -0,0 +1,16 @@
+package figura;
+
+public abstract class Figura {
+
+    protected String nombre;
+
+    public Figura(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public abstract double calcularArea();
+
+    public String getNombre() {
+        return nombre;
+    }
+}
